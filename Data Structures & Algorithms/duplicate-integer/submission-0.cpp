d500ded84021a1af22c6bfba52577c1f155aa87c
tstx <71097288+tstx@users.noreply.github.com>
2026-08-28 03:34:55 +0200

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::set<int> unique_nums;
+        for (int num : nums) {
+            bool inserted;
+            std::tie(std::ignore, inserted) = unique_nums.insert(num);
+            if (!inserted) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
